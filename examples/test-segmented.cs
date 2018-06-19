74ab9f24cc6742f594c045d32cf70195d298e9c8
Jeff Thompson <jefft0@remap.ucla.edu>
2018-06-19 15:36:56 -0700

    examples: In test-segmented, change the demo host to memoria.

--- examples/test-segmented.cs
+++ examples/test-segmented.cs
@@ -33,8 +33,7 @@ namespace TestCnlDotNet {
     static void
     Main(string[] args)
     {
-      // Connect to the demo host at memoria.ndn.ucla.edu .
-      var face = new Face("128.97.98.8");
+      var face = new Face("memoria.ndn.ucla.edu");
       var page = new Namespace
         ("/ndn/edu/ucla/remap/demo/ndn-js-test/named-data.net/project/ndn-ar2011.html/%FDT%F7n%9E");
       page.setFace(face);
